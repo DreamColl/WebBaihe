55c9a0e4dc19e3d6fc61ca4b58bb04e86a41923e
xiong <xiongjiyuan@163.com>
2017-01-07 12:12:02 +0800

    表单完成，样式TODO，接口TODO

--- baihe_web/src/components/Entry.vue
+++ baihe_web/src/components/Entry.vue
@@ -1,28 +1,77 @@
 <template>
-  <form action="post">
-    <div class="entry">
+  <div class="entry">
+    <form action="post">
       <div v-for="(item,index) in items">
+        <!--input-->
         <a>{{ item.question }}</a>
-        <input v-bind:type="item.type" ></input>
+        <input :type="item.type" v-if="item.type == 'input' "></input>
+        <!--text-->
+        <textarea :type="item.type" v-if="item.type == 'textarea' "></textarea>
+        <!--select-->
+        <select v-if="item.type == 'select' ">
+          <option v-for="(op,index) in item.options">{{ op }}</option>
+        </select>
+        <!--checkbox-->
+        <a v-if="item.type == 'checkbox' " :value="op" v-for="(op,index) in item.options">
+          <label for="">{{ op }}</label>
+          <input :type="item.type">
+          </input>
+        </a>
+        <!--radio-->
+        <a v-if="item.type == 'radio' " v-for="(op,index) in item.options" v-model="picked">
+          <label :for="op">{{ op }}</label>
+          <input :value="op" :name="item.question" :type="item.type">
+          </input>
+        </a>
+        <!--<span v-if="item.type == 'radio'">Picked: {{ item.picked }}</span>-->
+        <!--<input :type="item.type" v-if="item.type == 'radio' " :value="op" v-for="(op,index) in item.options"> {{ op }}-->
+        <!--file-->
+        <input :type="item.type" v-if="item.type == 'file'">
       </div>
-    </div>
-  </form>
+    </form>
+    <input type="submit">
+  </div>
 </template>
 <script>
 export default {
   name: 'entry',
   data() {
     return {
-      items: [{
-          type: 'input',
-          question: '队名'
-        },
-        {
-          type: 'input',
-          question: '队名'
-        }
-      ]
+      items: data
     }
+  },
+  methods: {
+
   }
 }
+
+var data = [{
+    type: 'input',
+    question: '队名'
+  },
+  {
+    type: 'textarea',
+    question: '测试多行文本'
+  },
+  {
+    type: 'select',
+    question: '地区',
+    options: ['华北', '东部', '中部', '西部', '东北']
+  },
+  {
+    type: 'checkbox',
+    question: '地区(复选)：',
+    options: ['华北', '东部', '中部', '西部', '东北'],
+  },
+  {
+    type: 'radio',
+    question: '地区（单选）：',
+    options: ['华北', '东部', '中部', '西部', '东北'],
+    picked: '双向绑定TODO',
+  },
+  {
+    type: 'file',
+    question: '上传文件：',
+  }
+]
 </script>
