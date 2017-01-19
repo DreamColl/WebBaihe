5be19fa06e63d36a4d8b09e2386291c805dd123e
shadyzoz <shadyzoz@163.com>
2017-01-19 13:53:12 +0800

    roll back admin

--- baihe_api/baihe_api/baihe_admin/admin.py
+++ baihe_api/baihe_api/baihe_admin/admin.py
@@ -2,15 +2,11 @@ from django.contrib import admin
 from baihe_api.apps.form_builder.models import BaiheForm, BaiheFormData
 
 
-# @admin.register(BaiheForm)
-# class FormAdmin(admin.ModelAdmin):
-#     pass
+@admin.register(BaiheForm)
+class FormAdmin(admin.ModelAdmin):
+    pass
 
 
-# @admin.register(BaiheFormData)
-# class FormDataAdmin(admin.ModelAdmin):
-#     pass
-
-
-admin.site.register(BaiheForm)
-admin.site.register(BaiheFormData)
+@admin.register(BaiheFormData)
+class FormDataAdmin(admin.ModelAdmin):
+    pass
